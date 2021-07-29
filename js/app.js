8ad54b93b58c324e33274b7ff5b9de0dee74218f
emory <dvanullnulldva@gmail.com>
2021-07-29 05:28:06 +0300

    conditions&cyclesTrain

--- js/app.js
+++ js/app.js
@@ -1,25 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
 
 const num = 55;
 
@@ -44,3 +25,62 @@ switch(num){
         console.log('error: неизвестная ошибка.');
         break;
 }
+
+
+// let abcd = 10;
+
+// // while(abcd < 15){
+// //     console.log(abcd);
+// //     abcd++;
+// // }
+
+// do{
+//     console.log('abcd');
+//     abcd++;
+// }
+// while(abcd < 15)
+
+// for(let i = 1; i <= 5; i++){
+
+//     if(i === 2){
+//         // break;
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMovieDB);
+
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('complete!')
+    }else{
+        console.log('error')
+        i--;
+    }
+
+}
+
+if(personalMovieDB.count < 10){
+    console.log('malo')
+} else if(personalMovieDB.count < 30){
+    console.log('normal')
+} else if(personalMovieDB.count < 50){
+    console.log('great!')
+}
+
