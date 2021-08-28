189952b14f6e6e14f6aaf59b1233fec8441d2d3e
emory <dvanullnulldva@gmail.com>
2021-08-28 21:43:17 +0300

    'node

--- js/app.js
+++ js/app.js
@@ -567,3 +567,25 @@ links.forEach(link => {
         console.log(e.target);
     }, {once: true})
 })
+
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('.add').parentNode);
+console.log(document.querySelector('.add').parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling)
+
+for(let node of document.body.childNodes){
+    if(node.nodeName =='#text'){
+        continue;
+    }
+    
+    console.log(node)
+}
