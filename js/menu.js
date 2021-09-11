136c3db166bbd16c598b02f1f50964a938d9c47d
emory <dvanullnulldva@gmail.com>
2021-09-11 21:22:52 +0300

    classListContainsTrain

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,21 @@
 const btns = document.querySelectorAll('button');
 
 // console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-console.log(btns[0].classList.add('journal'));
-console.log(btns[0].classList.item(2));
-console.log(btns[0].classList.remove('journal'));
-console.log(btns[0].classList.item(2));
+// // console.log(btns[0].classList.item(1));
+// console.log(btns[0].classList.add('journal'));
+// console.log(btns[0].classList.item(2));
+// console.log(btns[0].classList.remove('journal'));
+// console.log(btns[0].classList.item(2));
 
-console.log(btns[2].classList.add('red'));
-console.log(btns[4].classList.add('red'));
+// console.log(btns[2].classList.add('red'));
+// console.log(btns[4].classList.add('red'));
+
+// if(btns[2].classList.contains("red")){
+//     console.log('red')
+// }
+
+btns[0].addEventListener('click', ()=>{
+    if(!btn[1].classList.contains('red')){
+        btns[1].classList.add 
+    }
+}); 
